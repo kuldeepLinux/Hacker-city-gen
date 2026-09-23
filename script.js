5d4d79d904f6ff1c144f485a0b1c745fe351a5d1
Kuldeep <r24mys7wdk@privaterelay.appleid.com>
2026-09-24 01:47:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  document.getElementById('boot').textContent = 'TEST OK';
+  document.getElementById('tabs').style.display = 'flex';
+  document.getElementById('tab-gen').style.display = 'block';
+});
