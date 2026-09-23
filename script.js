358edd8cc3a2744f75a46d2cafd1b53bce2d055f
Kuldeep <r24mys7wdk@privaterelay.appleid.com>
2026-09-24 01:55:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-  document.getElementById('boot').textContent = 'TEST OK BHAI CHAL GAYA';
-  document.getElementById('tabs').style.display = 'flex';
-  document.getElementById('tab-gen').style.display = 'block';
-  document.getElementById('boot').style.color = 'red';
-});
