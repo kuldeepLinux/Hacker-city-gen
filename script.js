09f7ec75c81155a15f4ade4b79971937ebec3f95
Kuldeep <r24mys7wdk@privaterelay.appleid.com>
2026-09-24 01:50:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  document.getElementById('boot').textContent = 'TEST OK BHAI CHAL GAYA';
+  document.getElementById('tabs').style.display = 'flex';
+  document.getElementById('tab-gen').style.display = 'block';
+  document.getElementById('boot').style.color = 'red';
+});
