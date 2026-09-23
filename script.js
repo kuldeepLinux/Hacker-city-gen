a79e2da65daf563a0a4d7d99b2803e9f40806714
Kuldeep <r24mys7wdk@privaterelay.appleid.com>
2026-09-24 01:50:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-  document.getElementById('boot').textContent = 'TEST OK';
-  document.getElementById('tabs').style.display = 'flex';
-  document.getElementById('tab-gen').style.display = 'block';
-});
